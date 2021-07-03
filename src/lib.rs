f9c1fe9e2dded6faa55db1c3b804050980183673
Daan4 <Daan4@users.noreply.github.com>
2021-07-03 18:16:12 +0200

    update lib

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,12 @@ pub mod wallet;
 pub mod account;
 pub mod pool;
 pub mod seed;
+pub mod address;
 pub mod unit;
 pub mod common;
+
+pub mod communication {
+    pub mod rpc;
+    pub mod sim;
+    pub mod ws;
+}
