67c7636686587ad680010850449e0c7263d0bb8d
Daan4 <Daan4@users.noreply.github.com>
2021-07-03 18:14:31 +0200

    some rpc mock functions

---
+++ src/communication/rpc.rs
@@ -0,0 +1,17 @@
+use crate::unit::Raw;
+
+fn rpc_block_create() {
+
+}
+
+pub fn rpc_account_balance(address: &String) -> (Raw, Raw) {
+    (0, 0)
+}
+
+pub fn rpc_send() {
+
+}
+
+pub fn rpc_receive() {
+
+}
