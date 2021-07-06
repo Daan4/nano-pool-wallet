64d7d1ff81c7cb7ef610810d6ea538505f6f1412
Daan4 <Daan4@users.noreply.github.com>
2021-07-06 20:18:00 +0200

    implement rpc post

--- src/communication/rpc.rs
+++ src/communication/rpc.rs
@@ -1,11 +1,58 @@
+use serde_json::{json, Value};
+use curl::easy::Easy;
+use std::io::Read;
+
 use crate::unit::Raw;
+use crate::address::Address;
 
-fn rpc_block_create() {
+fn rpc(json: &Value) -> Result<Value, String> {
+    println!("RPC send {}", json);
+    let data = json.to_string();
+    let mut data = data.as_bytes();
+    let mut easy = Easy::new();
+    easy.url("127.0.0.1:17076").unwrap();
+    easy.post(true).unwrap();
+    easy.post_field_size(data.len() as u64).unwrap();
 
+    let mut dst = Vec::new();
+    let mut transfer = easy.transfer();
+    transfer.read_function(|buf| {
+        Ok(data.read(buf).unwrap_or(0))
+    }).unwrap();
+    transfer.write_function(|data| {
+        dst.extend_from_slice(data);
+        Ok(data.len())
+    }).unwrap();
+    transfer.perform().unwrap();
+    drop(transfer);
+    let dst = String::from_utf8(dst).unwrap();
+    let dst: Value = serde_json::from_str(&dst).unwrap();
+    println!("RPC recv {}", dst);
+    Ok(dst)
 }
 
-pub fn rpc_account_balance(address: &String) -> (Raw, Raw) {
-    (0, 0)
+pub fn rpc_account_balance(address: &Address) -> Result<(Raw, Raw), String> {
+    let message = json!({
+        "action": "account_balance",
+        "account": address
+    });
+    let response = rpc(&message)?;
+
+    match (response["balance"].clone(), response["pending"].clone()) {
+        (Value::Number(balance), Value::Number(pending)) => {
+            match (balance.as_u64(), pending.as_u64()) {
+                (Some(balance), Some(pending)) => {
+                    Ok((balance as Raw, pending as Raw))
+                },            
+                (_, _) => {        
+                    Err(format!("RPC invalid response {} to message {}", response, message))
+            	}
+            }
+        },
+        (_, _) => {
+            Err(format!("RPC invalid response {} to message {}", response, message))
+        }
+    }
 }
 
 pub fn rpc_send() {
